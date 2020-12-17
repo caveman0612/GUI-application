4cdac65eb5416ed7272a3e5b7c68ac17fb47e55b
caveman0612 <kyle.stinar@hotmail.com>
2020-12-16 18:34:22 -0800

    removed text and add buttons

--- basic window.py
+++ basic window.py
@@ -1,6 +1,22 @@
 from tkinter import *
 
 root =Tk()
-theLabel = Label(root, text="this is the words I want to say on here")
-theLabel.pack()
+
+topframe = Frame(root)
+topframe.pack()
+
+bottomframe = Frame(root)
+bottomframe.pack(side=BOTTOM)
+
+button1 = Button(topframe, text="click here B!", fg="red")
+button2 = Button(topframe, text="click here B2", fg="blue")
+button3 = Button(topframe, text="click here B3", fg="green")
+button4 = Button(bottomframe, text="click here B4", fg="purple")
+
+button1.pack(side=LEFT)
+button2.pack(side=RIGHT)
+button3.pack()
+button4.pack()
+
 root.mainloop()
+
