102a972aff81b89a241b660f0a7efcde649afca2
caveman0612 <kyle.stinar@hotmail.com>
2020-12-17 14:06:14 -0800

    removed text and add buttons

--- starter window.py
+++ starter window.py
@@ -2,5 +2,13 @@ from tkinter import *
 
 root =Tk()
 
+one = Label(root, text="one thing", bg="red", fg="white")
+one.pack()
+
+two = Label(root, text="another thing", bg="blue", fg="black")
+two.pack(fill=X)
+
+three = Label(root, text="same thing", bg="yellow", fg="black")
+three.pack(side=LEFT, fill=Y)
 
 root.mainloop()
