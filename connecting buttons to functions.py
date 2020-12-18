fa8229c1027cfed2ecd2e19f87c543d272241d0a
caveman0612 <75807458+caveman0612@users.noreply.github.com>
2020-12-18 12:49:36 -0800

    connected multiple buttons to different clicks

--- connecting buttons to functions.py
+++ connecting buttons to functions.py
@@ -2,15 +2,24 @@ from tkinter import *
 
 root =Tk()
 
-def printname(event):
-    print("hello")
+def leftClick(event):
+    print('left')
 
 
+def middleClick(event):
+    print("Middle")
 
-button_1 = Button(root, text='Hello')
-button_1.bind("<Button-1>", printname)
-#"<Button-1>" is for left button
-button_1.pack()
+
+def rightClick(event):
+    print("Right")
+
+frame = Frame(root, width=300, height=250)
+frame.bind("<Button-1>", leftClick)
+frame.bind("<Button-2>", middleClick)
+frame.bind("<Button-3>", rightClick)
+frame.pack()
+
+
 
 root.mainloop()
 
