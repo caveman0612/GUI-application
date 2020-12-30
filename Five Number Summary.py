c69df5ba4c195964833efa8db1624e98aa838554
caveman0612 <75807458+caveman0612@users.noreply.github.com>
2020-12-30 12:03:56 -0800

    solved

--- Five Number Summary.py
+++ Five Number Summary.py
@@ -7,7 +7,38 @@
 # Maximum
 
 
-def five_num(array)
+def five_num(array):
+    array.sort()
+    minimum = min(array)
+    first_qtr = 0
+    med = 0
+    third_qtr = 0
+    maximum = max(array)
+
+    if len(array) % 2 == 0:
+        med_1_index = int((len(array)-1) / 2)
+        med_2_index = int(len(array)/2)
+        med_2 = array[int(len(array)/2)]
+        med_1 = array[int((len(array)-1) / 2)]
+        med = (med_1 + med_2)/2
+
+        #deals with 1st and 3rd quartile oif array is even
+        lower_list = array[:(med_1_index + 1)]
+        upper_list = array[med_2_index:]
+        first_qtr = lower_list[int((len(lower_list) - 1)/2)]
+        third_qtr = upper_list[int((len(lower_list) - 1) / 2)]
+
+    else:
+        med = (len(array) - 1)
+
+        lower_list = array[:int((len(array) + 1)/2)]
+        upper_list = array[int(len(array) / 2):]
+        first_qtr = lower_list[int((len(lower_list) - 1) / 2)]
+        third_qtr = upper_list[int((len(lower_list) - 1) / 2)]
+
+    number_summary = [minimum, first_qtr, med, third_qtr, maximum]
+    return number_summary
 
 
-end
+
+five_num([4, 6, -6, 7, -3, 5, -2])
